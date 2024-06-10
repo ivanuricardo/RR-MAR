3e37ff921db4d3d25ba90aa1fed8f13bf7b54a12
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-06-10 16:20:12 +0200

    finish transitioning to (4,5)

--- plots/projheatmaps.jl
+++ plots/projheatmaps.jl
@@ -7,13 +7,13 @@ using CairoMakie
 
 globaldata = load("data/globaldata.rda")
 unpermuted = globaldata["matdata"]
-mardata = permutedims(unpermuted, (3, 2, 1))
+mardata = permutedims(unpermuted, (2, 3, 1))
 
 maxiter = 500
 tucketa = 1e-03
 ϵ = 1e-03
 
-tuckest = tuckerreg(mardata, [1, 3, 3, 4]; eta=tucketa, maxiter, p=1, ϵ)
+tuckest = tuckerreg(mardata, [3, 1, 4, 3]; eta=tucketa, maxiter, p=1, ϵ)
 
 cendata = mardata .- mean(mardata, dims=3)
 origy, lagy = tlag(cendata, 1);
@@ -27,7 +27,7 @@ xticksize = 15
 ylabsize = 15
 yticksize = 15
 
-fig = Figure(backgroundcolor=:transparent, resolution=(1200, 375));
+fig = Figure(backgroundcolor=:transparent, size=(1200, 375));
 countries = ["USA", "CAN", "DEU", "FRA", "GBR"]
 inds = ["IR", "GDP", "PROD", "CPI"]
 
@@ -35,8 +35,8 @@ ax1 = Axis(fig[1, 1], title="Resp: Indicators", titlesize=titlesize, xticks=(1:l
 ax1.yticks = (1:length(inds), inds)
 ax1.xticks = (1:length(inds), inds)
 
-heatmap!(ax1, tuckest.U[2] * tuckest.U[2]', colormap=:PuBu)
-hm1 = heatmap(tuckest.U[2] * tuckest.U[2]', colormap=:PuBu);
+heatmap!(ax1, tuckest.U[1] * tuckest.U[1]', colormap=:PuBu)
+hm1 = heatmap(tuckest.U[1] * tuckest.U[1]', colormap=:PuBu);
 
 Colorbar(fig[2, 1], hm1.plot, vertical=false)
 
@@ -44,8 +44,8 @@ ax2 = Axis(fig[1, 2], title="Resp: Countries", xticks=(1:length(countries), coun
 ax2.yticks = (1:length(countries), countries)
 ax2.xticks = (1:length(countries), countries)
 
-heatmap!(ax2, tuckest.U[1] * tuckest.U[1]', colormap=:PuBu)
-hm1 = heatmap(tuckest.U[1] * tuckest.U[1]', colormap=:PuBu);
+heatmap!(ax2, tuckest.U[2] * tuckest.U[2]', colormap=:PuBu)
+hm1 = heatmap(tuckest.U[2] * tuckest.U[2]', colormap=:PuBu);
 
 Colorbar(fig[2, 2], hm1.plot, vertical=false, ticks=[0.18, 0.21, 0.24, 0.27])
 
@@ -53,8 +53,8 @@ ax3 = Axis(fig[1, 3], title="Pred: Indicators", xticks=(1:length(inds), inds), y
 ax3.yticks = (1:length(inds), inds)
 ax3.xticks = (1:length(inds), inds)
 
-heatmap!(ax3, tuckest.U[4] * tuckest.U[4]', colormap=:PuBu)
-hm4 = heatmap(tuckest.U[4] * tuckest.U[4]', colormap=:PuBu);
+heatmap!(ax3, tuckest.U[3] * tuckest.U[3]', colormap=:PuBu)
+hm4 = heatmap(tuckest.U[3] * tuckest.U[3]', colormap=:PuBu);
 
 Colorbar(fig[2, 3], hm4.plot, vertical=false)
 
@@ -62,8 +62,8 @@ ax4 = Axis(fig[1, 4], title="Pred: Countries", xticks=(1:length(countries), coun
 ax4.yticks = (1:length(countries), countries)
 ax4.xticks = (1:length(countries), countries)
 
-heatmap!(ax4, tuckest.U[3] * tuckest.U[3]', colormap=:PuBu)
-hm3 = heatmap(tuckest.U[3] * tuckest.U[3]', colormap=:PuBu);
+heatmap!(ax4, tuckest.U[4] * tuckest.U[4]', colormap=:PuBu)
+hm3 = heatmap(tuckest.U[4] * tuckest.U[4]', colormap=:PuBu);
 
 Colorbar(fig[2, 4], hm3.plot, vertical=false)
 
